82744a99bae137168f29eb9c13660bf4cb956029
JimmyCodeMunity <jameswafula2002@gmail.com>
2024-04-27 15:45:39 +0300

    latest

--- screens/ResetPassword.jsx
+++ screens/ResetPassword.jsx
@@ -47,7 +47,7 @@ const ResetPassword = ({ navigation, route }) => {
   }, []);
 
   const getUserdata = () => {
-    fetch('https://opasso-app-backend.vercel.app/api/user/usersdata')
+    fetch('https://res-server-sigma.vercel.app/api/user/usersdata')
       .then((response) => response.json())
       .then((data) => {
         const user = data.find((item) => item.email === email);
@@ -66,7 +66,7 @@ const ResetPassword = ({ navigation, route }) => {
       alert("password do not match")
     } else {
       try {
-        const response = await axios.put(`https://opasso-app-backend.vercel.app/api/user/updatepassword/${email}`, {
+        const response = await axios.put(`https://res-server-sigma.vercel.app/api/user/updatepassword/${email}`, {
           password: password
         });
         setUserdata(response.data);
